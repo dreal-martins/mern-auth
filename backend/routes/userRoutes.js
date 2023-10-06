b233d9a616f5d26704e6d67186211b8e6ec4facc
Alexander Martins <alexandermartins123@icloud.com>
2023-10-06 20:22:01 +0100

    Initial commit

---
+++ backend/routes/userRoutes.js
@@ -0,0 +1,20 @@
+import express from "express";
+const router = express.Router();
+import {
+  authUser,
+  registerUser,
+  logoutUser,
+  getUserProfile,
+  updateUserProfile,
+} from "../controllers/userController.js";
+import protect from "../middleware/authMiddleware.js";
+
+router.post("/", registerUser);
+router.post("/auth", authUser);
+router.post("/logout", logoutUser);
+router
+  .route("/profile")
+  .put(protect, updateUserProfile)
+  .get(protect, getUserProfile);
+
+export default router;
---
+++ backend/server.js
@@ -0,0 +1,34 @@
+import path from "path";
+import express from "express";
+import dotenv from "dotenv";
+dotenv.config();
+const port = process.env.PORT || 5000;
+import cookieParser from "cookie-parser";
+import { notFound, errorHandler } from "./middleware/errorMiddleware.js";
+import connectDB from "./config/db.js";
+import userRoutes from "./routes/userRoutes.js";
+
+connectDB();
+
+const app = express();
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
+
+app.use("/api/users", userRoutes);
+
+if (process.env.NODE_ENV === "production") {
+  const __dirname = path.resolve();
+  app.use(express.static(path.join(__dirname, "frontend/build")));
+
+  app.get("*", (req, res) =>
+    res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"))
+  );
+} else {
+  app.get("/", (req, res) => res.send(`Server is running`));
+}
+
+app.use(notFound);
+app.use(errorHandler);
+
+app.listen(port, () => console.log(`Server started on ports ${port}`));
